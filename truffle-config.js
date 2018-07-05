ed2c8069a2c046f9bed553be594f02bc40b910f0
ZentaChainAdmin <40530024+ZentaChainAdmin@users.noreply.github.com>
2018-07-06 01:27:05 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   solc: {
     optimizer: {
       enabled: true,
